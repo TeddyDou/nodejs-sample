5a48f3e89f16715b18deca53060232ef8b85a4f8
Konstantinos Psimoulis <kpsimoulis@summit-tech.ca>
2018-02-03 13:47:45 -0500

    Added variable hello

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Load express framework and http server
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -7,10 +8,12 @@ app.use(express.static('assets'));
 app.set('view engine', 'ejs');
 
 
+// Main page
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Node.js Tutorial' });
+    res.render('index', { title: 'Node.js Tutorial', hello: "Hello Dear Node.js" });
 });
 
+// Extra pages
 app.get('/about.html', function (req, res) {
     res.send("This is my about page");
 });
@@ -19,6 +22,7 @@ app.get('/contact.html', function (req, res) {
     res.send("This is my contact page");
 });
 
+// Create server on port 8000
 http.listen(8000, function () {
     console.log('version 0.0.1 listening on *:8000');
 });
